cdbc6fe83e7753033ebab75ad566a4a9d6620a5a
Pablo Duce <paduzara@gmail.com>
2020-12-29 09:56:41 +0100

    final v2

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+#makeCacheMatrix calculates the inverse of the matrix and stores
+#it in a vector x, its stored in a different environnement from
+#cacheSolve, wich acts as a sort of cache
 
 makeCacheMatrix <- function(x = matrix()) {
   s <- NULL
@@ -10,14 +9,16 @@ makeCacheMatrix <- function(x = matrix()) {
     s <<- NULL
   }
   get <- function() x
-  setinverse <- function() s <<- solve(x)
-  getinverse <- function() m
+  setinverse <- function(solve) s <<- solve
+  getinverse <- function() s
   list(set = set, get = get,
        setinverse = setinverse,
        getinverse = getinverse)
 }
-## Write a short comment describing this function
-
+#cacheSolve searches in the global environnement for
+#x and looks at the getinverse component to see if
+#the inverse of this matrix was stored already, if not, it will calculate it an
+#store it in the cache
 cacheSolve <- function(x, ...) {
   s <- x$getinverse()
   if(!is.null(s)) {
